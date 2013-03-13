96f8273eaf980f07748fc264128440e089d5c37d
Rogers Dwiputra <setiady.rogers28@gmail.com>
2013-03-14 06:49:36 +0700

    benerin flow lelang (belum beres semua)
    benerin flow lelang, yang belum beres :
    - handling golden periode
    - handling jika lelang dibatalkan (harga lelang tidak mencapai harga
    milimal lelang)

--- application/controllers/user/lelang.php
+++ application/controllers/user/lelang.php
@@ -97,8 +97,9 @@ Class lelang extends CI_Controller{
 	//ajax, responsenya json status lelang tsb
 	public function get_status($id){
 		$lelang = $this->M_lelang->get_lelang($id);
+		$response['status'] = 'true';
 		//cek apakah lagi masa absensi atau sedang berlangsung
-			if($lelang['waktu_selesai'] > time()){
+			if($lelang['waktu_selesai'] - 600 > time()){
 				//masih waktu absensi
 
 				//get bidder yg udah absen
@@ -107,116 +108,104 @@ Class lelang extends CI_Controller{
 				for ($i=0; $i < count($absen); $i++) { 
 					$absen[$i]['bid'] = $this->cart->format_number($absen[$i]['bid']);
 				}
-				$response['status'] = 'true';
-				$response['msg'] = 'masa absensi';
+				$response['msg'] = 'Masa Absensi';
 				$response['bidder'] = $absen;
-
-				//get harganya & waktu sisa absen
-				$lelang = $this->M_lelang->get_lelang($id);
 				$response['time'] = $lelang['waktu_selesai'] - time();
-			}//end if lelang waktu selesai
+			}//end if lelang['waktu_selesai'] - 600 > time()
 			else{
-				//lelang sedang berlangsung
-					//cek apakah sudah ada periode
-					if($this->M_lelang->is_periode_exist($id)){
-						//periode sudah ada
-						//get periode terbaru
-						$periode = $this->M_lelang->get_periode_terbaru($id);
-						//cek periode masih berlaku atau ngga
-						if($periode['expired'] > time()){
-							//periode masih berlaku
-							$response['periode'] = 'masih berlaku '.$periode['id_periode_lelang'].' '.$periode['periode'];
-							//get bidder yg ngebid di periode itu
-							$bidder = $this->M_lelang->get_bidder($periode['id_periode_lelang']);
-							$response['status'] = 'true';
-							$response['msg'] = 'periode masih berlaku';
-							$response['bidder'] = $bidder;
-							//get harganya dan siswa waktu periode
-							//get harganya & waktu sisa waktu periode
-							$lelang = $this->M_lelang->get_lelang($id);
-							$response['time'] = $periode['expired'] - time();
-						}//end if periode expired
-						else{
-							//periode sudah habis
-							//cek jumlah org yg ada di periode, jika cuman ada 1 org berarti selesai
-							if($this->M_lelang->get_jmlh_bidder_periode($periode['id_periode_lelang']) < 2){
-								//hanya ada 1 orang, selesai lelangnya
-
-								//get bidder yg ngebid
-								$bidder = $this->M_lelang->get_bidder($periode['id_periode_lelang']);
-								$response['status'] = 'true';
-								$response['msg'] = 'lelang berakhir';
-								$response['bidder'] = $bidder;
-
-								//get harganya & waktu sisa waktu periode
-								$lelang = $this->M_lelang->get_lelang($id);
-								$response['time'] = $periode['expired'] - time();
-
-								//cek apakah ada pemenang?
-								if($this->M_lelang->get_pemenang($id) != false){
-									$pemenang = $this->M_lelang->get_pemenang($id);
-									//cek apakah sudah di insert
-									if($this->M_menang_lelang->is_pemenang_exist($id,$pemenang['id_user']) == false){
-										//belum di insert
-										$response['msg'] = 'ok';
-										$ikut_lelang = $this->M_ikut_lelang->get_ikut_lelang($id,$pemenang['id_user']);
-										//insert ke tabel menang lelang
-										$menang = array(
-											'id_ikut_lelang' => $ikut_lelang['id_ikut_lelang'],
-											'harga' => $lelang['harga_min'],
-											);
-										$this->M_menang_lelang->insert($menang);
-									}
+				//bukan masa absensi
+				//cek apakah golden periode atau bukan ?
+				if($lelang['golden_periode'] < time() && $lelang['golden_periode'] != 0){
+					//golden periode
+					//cek golden periode masih aktif atau ngga
+					if($lelang['golden_periode'] + 600 < time()){
+						//golden periode masih aktif
+						$response['msg'] = 'Golden Periode';
+					}
+					else{
+						//golden periode sudah ngga aktif, lelang selesai
+						//cek apakah harga lelang sudah mencapai harga minimal
+						if($lelang['harga_min'] >= $lelang['harga_max']){
+							//lelang valid
+							//cek apakah ada pemenang?
+							if($this->M_lelang->get_pemenang($id) != false){
+							$pemenang = $this->M_lelang->get_pemenang($id);
+								//cek apakah sudah di insert
+								if($this->M_menang_lelang->is_pemenang_exist($id,$pemenang['id_user']) == false){
+									//belum di insert
+									$response['msg'] = 'ok';
+									$ikut_lelang = $this->M_ikut_lelang->get_ikut_lelang($id,$pemenang['id_user']);
+									//insert ke tabel menang lelang
+									$menang = array(
+										'id_ikut_lelang' => $ikut_lelang['id_ikut_lelang'],
+										'harga' => $lelang['harga_min'],
+										);
+									$this->M_menang_lelang->insert($menang);
 								}
 							}
-							else{
-								//ada lebih dari 1 orang di periode tsb, buat periode baru
-								//lama waktunya jumlah orang di periode tsb * 6 detik
-								$jmlh_bidder = $this->M_lelang->get_jmlh_bidder_periode($periode['id_periode_lelang']);
-								//$jmlh_bidder = 3;
-								$expired = time() + 6 * $jmlh_bidder;
-								$data = array(
-									'periode' => $periode['periode'] + 1,
-									'expired' => $expired,
-									'id_lelang' => $id
-									);
-								$this->M_lelang->insert_periode($data);
-								//get bidder yg ngebid
-								$bidder = $this->M_lelang->get_bidder($periode['id_periode_lelang']);
-								$response['status'] = 'true';
-								$response['msg'] = 'periode '.$periode['id_periode_lelang'].' habis, buat periode '.$data['periode'];
-								$response['bidder'] = $bidder;
-
-								//get harganya & waktu sisa waktu periode
-								$lelang = $this->M_lelang->get_lelang($id);
-								$response['time'] = $expired - time();
+						}
+						else{
+							//lelang dibatalkan
+							//kembalikan semua point user
+							//hapus dari database absen & tawar
+						}
+						$response['msg'] = 'Lelang Berakhir';
+					}
+					$response['time'] = $lelang['golden_periode'] + 600 - time();
+				}
+				else{
+					//bukan golden periode
+					//cek apakah sudah berakhir
+					if($lelang['waktu_selesai'] < time()){
+						//lelang selesai
+						//set golden periode ke 0
+						//cek apakah harga lelang sudah mencapai harga minimal
+						if($lelang['harga_min'] >= $lelang['harga_max']){
+							//lelang valid
+							//cek apakah ada pemenang?
+							if($this->M_lelang->get_pemenang($id) != false){
+							$pemenang = $this->M_lelang->get_pemenang($id);
+								//cek apakah sudah di insert
+								if($this->M_menang_lelang->is_pemenang_exist($id,$pemenang['id_user']) == false){
+									//belum di insert
+									$response['msg'] = 'ok';
+									$ikut_lelang = $this->M_ikut_lelang->get_ikut_lelang($id,$pemenang['id_user']);
+									//insert ke tabel menang lelang
+									$menang = array(
+										'id_ikut_lelang' => $ikut_lelang['id_ikut_lelang'],
+										'harga' => $lelang['harga_min'],
+										);
+									$this->M_menang_lelang->insert($menang);
+								}
+								$response['msg'] = 'Lelang Berakhir dengan pemenang id_user : '.$pemenang['id_user'];
 							}
-						}//end else periode expired
-					}//end if periode exist
+						}
+						else{
+							//lelang dibatalkan
+							//kembalikan semua point user
+							//get bidder yg udah absen
+							$ikut_lelang = $this->M_lelang->get_ikut_lelang($id);
+							//foreach masing2 user
+							foreach ($ikut_lelang as $key) {
+								//echo 'id_user : '.$key['id_user'].' jumlah bid : '.$this->M_lelang->get_jumlah_bid_user($key['id_ikut_lelang']).' Point habis : '.$lelang['point_bid'] * $this->M_lelang->get_jumlah_bid_user($key['id_ikut_lelang']).'<br/>';
+							}
+							//hapus dari database absen & tawar
+							$response['msg'] = 'Lelang Dibatalkan';
+						}
+					}
 					else{
-						//periode belum ada, maka buat periode baru
-						//get jumlah orang yg absen * 600
-						$jmlah_absen = $this->M_lelang->get_jmlh_absen($id);
-						$expired = time() + 6 * $jmlah_absen;
-						$data = array(
-							'periode' => 1,
-							'expired' => $expired,
-							'id_lelang' => $id
-							);
-						$this->M_lelang->insert_periode($data);
-						//get periode terbaru
-						$periode = $this->M_lelang->get_periode_terbaru($id);
-						//get bidder yg ngebid
-						$bidder = $this->M_lelang->get_bidder($periode['id_periode_lelang']);
-						$response['status'] = 'true';
-						$response['msg'] = 'buat periode';
-						$response['bidder'] = $bidder;
-
-						//get harganya & waktu sisa waktu periode
-						$lelang = $this->M_lelang->get_lelang($id);
-						$response['time'] = $periode['expired'] - time();
-					}//end else periode exist
-			}//end else lelang waktu selesai
+						$response['msg'] = 'Lelang Sedang Berlangsung';
+					}
+					$response['time'] = $lelang['waktu_selesai'] - time();
+				}
+				//get bidder
+				$bidder = $this->M_lelang->get_bidder($id);
+				//format currency jadi di frontend tinggal tampilin aja
+				for ($i=0; $i < count($bidder); $i++) { 
+					$bidder[$i]['tawar'] = $this->cart->format_number($bidder[$i]['tawar']);
+				}
+				$response['bidder'] = $bidder;
+			}
 		$response['harga'] = $this->cart->format_number($lelang['harga_min']);
 		$response['hemat'] = $this->cart->format_number($lelang['harga_pasar'] - $lelang['harga_min']);
 		$response['pemenang'] = $this->M_lelang->get_pemenang($id);
@@ -228,7 +217,7 @@ Class lelang extends CI_Controller{
 		//cek udah login atau belum
 		if(is_user()){
 			$lelang = $this->M_lelang->get_lelang($id);
-			if($lelang['waktu_selesai'] > time()){
+			if($lelang['waktu_selesai'] - 600 > time()){
 				//masih waktu absensi
 				if($this->M_ikut_lelang->cek_absen($id,$this->session->userdata('id_user')) != 'true'){
 					//absen!
@@ -247,10 +236,8 @@ Class lelang extends CI_Controller{
 						$this->M_user->update_saldo($this->session->userdata('id_user'),$data);
 
 						//naikkan harga sesuai kenaikan harga per bid
-						$harga_baru = $lelang['harga_min'] + $lelang['kenaikan_harga'];
-						//cek harga yg naik < harga max
-						if($harga_baru < $lelang['harga_max']){
-							//update harganya
+						if($lelang['harga_min'] < $harga_max['harga_max']){
+							$harga_baru = $lelang['harga_min'] + $lelang['kenaikan_harga'];
 							$update_harga = array(
 								'harga_min' => $harga_baru
 								);
@@ -284,86 +271,51 @@ Class lelang extends CI_Controller{
 				//cek apakah user udah absen
 				if($this->M_ikut_lelang->cek_absen($id,$this->session->userdata('id_user')) == true){
 					//sudah absen
-					//get periode terbaru
-					$periode = $this->M_lelang->get_periode_terbaru($id);
-					//cek periode masih aktif atau ngga
-					if($periode['expired'] > time()){
-						//jika bukan periode 1
-						if($periode['periode'] != 1){
-							//cek apakah user ada di periode sebelumnya
-							$periode_sebelumnya = $this->M_lelang->get_periode_sebelumnya($periode['periode'],$id);
-							if($this->M_lelang->is_user_exist_on_periode($periode_sebelumnya['id_periode_lelang'],$this->session->userdata('id_user'))){
-								//user ada di periode sebelumnya
-								//cek saldo user
-								$saldo_user = $this->M_user->get_saldo($this->session->userdata('id_user'));
-								if($saldo_user < $lelang['point_daftar']){
-									$response['status'] = 'false';
-									$response['msg'] = 'Saldo anda tidak cukup!';
-								}
-								else{
-									//kurangi point karena ngebid
-									$saldo = $saldo_user - $lelang['point_bid'];
-									$data = array(
-									'saldo' => $saldo,
-									);
-									$this->M_user->update_saldo($this->session->userdata('id_user'),$data);
-									//naikkan harga sesuai kenaikan harga perbid
-									$harga_baru = $lelang['harga_min'] + $lelang['kenaikan_harga'];
-									//cek harga yg naik < harga max
-									if($harga_baru < $lelang['harga_max']){
-										//update harganya
-										$update_harga = array(
-											'harga_min' => $harga_baru
-											);
-										$this->M_lelang->update($update_harga,$id);
-									}
-									else{
-										$harga_baru = $lelang['harga_min'];
-									}
-									//insert ke tabel log lelang
-									//get id_ikut_lelang
-									$ikut_lelang = $this->M_ikut_lelang->get_ikut_lelang($id,$this->session->userdata('id_user'));
-									$log_lelang = array(
-										'id_ikut_lelang' => $ikut_lelang['id_ikut_lelang'],
-										'id_periode_lelang' => $periode['id_periode_lelang'],
-										'bid' => $harga_baru
-										);
-									$this->M_lelang->insert_log_lelang($log_lelang);
-									//update periode aktif + 6 detik
-									$update_expired = array(
-										'expired' => $periode['expired'] + 6,
-										);
-									$this->M_lelang->update_periode($update_expired,$periode['id_periode_lelang']);
-									$response['status'] = 'true';
-									$response['msg'] = 'Berhasil Bid di Periode ke '.$periode['periode'];
-								}
-							}
-							else{
-								//user ngga ada di periode sebelumnya, ga boleh ngebid
-								$response['status'] = 'false';
-								$response['msg'] = 'Anda Tidak Terdaftar Di Periode '.$periode_sebelumnya['id_periode_lelang'].' '.$periode_sebelumnya['periode'].' !';
-							}
+					//cek apakah golden periode atau bukan ?
+					if($lelang['golden_periode'] < time() && $lelang['golden_periode'] != 0){
+						//golden periode
+						//cek golden periode masih aktif atau ngga
+						if($lelang['golden_periode'] + 600 < time()){
+							//golden periode masih aktif
+							$response['status'] = 'false';
+							$response['msg'] = 'Golden Periode';
 						}
 						else{
-							//baru periode 1
+							//golden periode sudah ngga aktif, lelang selesai
+							$response['status'] = 'false';
+							$response['msg'] = 'Golden Periode Selesai! Lelang Ini Sudah Berakhir!';
+						}
+					}
+					else{
+						//bukan golden periode
+						//cek apakah sudah berakhir
+						if($lelang['waktu_selesai'] < time()){
+							//lelang selesai
+							$response['status'] = 'false';
+							$response['msg'] = 'Lelang Ini Sudah Berakhir!';
+						}//end if lelang waktu selesai
+						else{
+							//lelang masih berjalan
 							//cek saldo user
 							$saldo_user = $this->M_user->get_saldo($this->session->userdata('id_user'));
-							if($saldo_user < $lelang['point_daftar']){
+							if($saldo_user < $lelang['point_bid']){
+								//saldo ngga cukup
 								$response['status'] = 'false';
 								$response['msg'] = 'Saldo anda tidak cukup!';
-							}
+							}//end if cek saldo user
 							else{
-								//kurangi point karena ngebid
+								//saldo cukup
+								//kurangi saldo user
+								//kurangi saldo user untuk daftar
 								$saldo = $saldo_user - $lelang['point_bid'];
-								$data = array(
+								$data_saldo = array(
 								'saldo' => $saldo,
 								);
-								$this->M_user->update_saldo($this->session->userdata('id_user'),$data);
-								//naikkan harga sesuai kenaikan harga perbid
-								$harga_baru = $lelang['harga_min'] + $lelang['kenaikan_harga'];
-								//cek harga yg naik < harga max
-								if($harga_baru < $lelang['harga_max']){
-									//update harganya
+								$this->M_user->update_saldo($this->session->userdata('id_user'),$data_saldo);
+
+								//naikkan harga sesuai kenaikan harga per bid
+								if($lelang['harga_min'] <= $harga_max['harga_max']){
+									$harga_baru = $lelang['harga_min'] + $lelang['kenaikan_harga'];
 									$update_harga = array(
 										'harga_min' => $harga_baru
 										);
@@ -372,35 +324,32 @@ Class lelang extends CI_Controller{
 								else{
 									$harga_baru = $lelang['harga_min'];
 								}
-								//insert ke tabel log lelang
-								//get id_ikut_lelang
+
+								//update waktu lelang + 6s
+								$update_waktu = $lelang['waktu_selesai'] + 6;
+								$this->M_lelang->update(array('waktu_selesai' => $update_waktu),$id);
+
+								//insert ke tabel tawar lelang
 								$ikut_lelang = $this->M_ikut_lelang->get_ikut_lelang($id,$this->session->userdata('id_user'));
-								$log_lelang = array(
+								$tawar = array(
 									'id_ikut_lelang' => $ikut_lelang['id_ikut_lelang'],
-									'id_periode_lelang' => $periode['id_periode_lelang'],
-									'bid' => $harga_baru
+									'tawar' => $harga_baru,
+									'waktu_tawar' => time(),
+									'golden_periode' => 0
 									);
-								$this->M_lelang->insert_log_lelang($log_lelang);
-								//update periode aktif + 6 detik
-								$update_expired = array(
-									'expired' => $periode['expired'] + 6,
-									);
-								$this->M_lelang->update_periode($update_expired,$periode['id_periode_lelang']);
+								$this->M_lelang->insert_tawar($tawar);
+
 								$response['status'] = 'true';
-								$response['msg'] = 'Berhasil Bid di Periode ke '.$periode['id_periode_lelang'];
-							}
-						}
-					}
-					else{
-						//periode sudah tidak aktif atau lelang sudah berakhir
-						$response['status'] = 'false';
-						$response['msg'] = 'Lelang Ini Sudah Berakhir!';
-					}
-				}
+								$response['msg'] = 'Tawar Berhasil';
+
+							}//end else cek saldo user
+						}//end else lelang waktu selesai < time()
+					}//end else golden periode
+				}//end if user absen
 				else{
+					//belum absen
 					$response['status'] = 'false';
 					$response['msg'] = 'Anda belum absen';
-					//belum absen
 				}
 			}
 		}
